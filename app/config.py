66af8a8dba1989056c7448604b1cd16589b17aca
matissevh19 <matisse.vanhoudt@ugent.be>
2025-12-16 16:49:51 +0100

    runtime error fix

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os 
 
 class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # NIEUW: Scheduler instellingen
