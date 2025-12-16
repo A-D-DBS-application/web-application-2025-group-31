c010c5838a41d148260f136c49ec2e21944be816
matissevh19 <matisse.vanhoudt@ugent.be>
2025-12-16 12:52:53 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Config:
     SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Tamat312025.@db.wghuuwjgvqzcdtuzzwyl.supabase.co:5432/postgres?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.wghuuwjgvqzcdtuzzwyl:Tamat312025.@aws-1-eu-central-1.pooler.supabase.com:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # NIEUW: Scheduler instellingen
     SCHEDULER_API_ENABLED = False  # Houd de API uit, we hebben die niet nodig
